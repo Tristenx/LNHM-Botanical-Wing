ae7721bf1d248d4d2751ee2c41790c448b4b4f5b
TristenX <1604tristenarron@gmail.com>
2025-09-23 22:18:25 +0100

    Produces a summary row for a plant

--- etl_rds_to_s3/transform.py
+++ etl_rds_to_s3/transform.py
@@ -12,6 +12,15 @@ def get_all_plant_ids(plants: list) -> list:
     return ids
 
 
+def get_id_map(plant_data: dict[list], subject: str) -> dict[int:str]:
+    """Returns a map of ids and names for a subject."""
+    subject_rows = plant_data[subject]
+    id_map = dict()
+    for row in subject_rows:
+        id_map[row[0]] = row[1]
+    return id_map
+
+
 def get_records_for_id(plant_id: int, recordings: list) -> pd.DataFrame:
     """Returns a Dataframe containing all the records for a plant."""
     records = []
@@ -29,11 +38,51 @@ def get_records_for_id(plant_id: int, recordings: list) -> pd.DataFrame:
     return pd.DataFrame(records)
 
 
+def get_row_by_id(desired_id: int, plant_data: dict[list], subject: str) -> tuple:
+    """Returns the row from the plant table which contains the plant id."""
+    rows = plant_data[subject]
+    for row in rows:
+        if row[0] == desired_id:
+            return row
+    return None
+
+
+def clean_plant_records(records: pd.DataFrame, plant_data: dict[list]) -> dict:
+    """Returns a dictionary containing a summary of the records."""
+    plant_map = get_id_map(plant_data, "plant")
+    botanist_map = get_id_map(plant_data, "botanist")
+    summary = dict()
+
+    summary["plant_id"] = records["plant_id"].iloc()[0]
+    summary["plant_name"] = records["plant_id"].map(plant_map).to_list()[0]
+    plant_info = get_row_by_id(summary["plant_id"], plant_data, "plant")
+    summary["scientific_name"] = plant_info[2]
+    summary["country"] = get_id_map(plant_data, "country")[plant_info[3]]
+    summary["city"] = get_id_map(plant_data, "city")[plant_info[4]]
+    summary["latitude"] = plant_info[5]
+    summary["longitude"] = plant_info[6]
+
+    summary["botanist_id"] = records["botanist_id"].iloc()[0]
+    summary["botanist_name"] = records["botanist_id"].map(botanist_map).to_list()[
+        0]
+    botanist_info = get_row_by_id(
+        summary["botanist_id"], plant_data, "botanist")
+    summary["botanist_email"] = botanist_info[2]
+    summary["botanist_phone_number"] = botanist_info[3]
+
+    summary["avg_temperature"] = records["temperature"].mean()
+    summary["avg_soil_moisture"] = records["soil_moisture"].mean()
+
+    summary["last_watered"] = records["last_watered"].max()
+    summary["date"] = records["recording_taken"].dt.date.iloc()[0]
+    return summary
+
+
 if __name__ == "__main__":
     load_dotenv()
     db_conn = get_connection()
-    plant_data = get_data(db_conn)
-    plant_ids = get_all_plant_ids(plant_data["plant"])
+    plant_tables = get_data(db_conn)
+    plant_ids = get_all_plant_ids(plant_tables["plant"])
     plant_record = get_records_for_id(
-        plant_ids[4], plant_data["recording"])
-    print(plant_record)
+        plant_ids[4], plant_tables["recording"])
+    print(clean_plant_records(plant_record, plant_tables))

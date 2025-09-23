92086497b693ba628b5affe082bdd923d53718e9
DemoyDW <demoywitter@gmail.com>
2025-09-23 08:52:02 +0100

    Testing direct install of pylint

---
+++ bad_code/bad_code.py
@@ -0,0 +1,14 @@
+"""good_code"""
+
+
+def add():
+    """returns the sum on 1 and 2"""
+
+    return 1+2
+
+
+BAD_CONSTANT = BAD
+
+
+if __name__ == "__main__":
+    add()
---
+++ good_code.py
@@ -0,0 +1,11 @@
+"""good_code"""
+
+
+def add():
+    """returns the sum on 1 and 2"""
+
+    return 1+2
+
+
+if __name__ == "__main__":
+    add()

ab20f5a9796be65a7070b71d9df1787a9d867e08
TristenX <1604tristenarron@gmail.com>
2025-09-23 17:05:46 +0100

    Gets a dataframe for all records of a plant

--- etl_rds_to_s3/transform.py
+++ etl_rds_to_s3/transform.py
@@ -12,8 +12,28 @@ def get_all_plant_ids(plants: list) -> list:
     return ids
 
 
+def get_records_for_id(plant_id: int, recordings: list) -> pd.DataFrame:
+    """Returns a Dataframe containing all the records for a plant."""
+    records = []
+    for recording in recordings:
+        if recording[0] == plant_id:
+            record = dict()
+            record["recording_id"] = recording[0]
+            record["plant_id"] = recording[1]
+            record["botanist_id"] = recording[2]
+            record["temperature"] = recording[3]
+            record["last_watered"] = recording[4]
+            record["soil_moisture"] = recording[5]
+            record["recording_taken"] = recording[6]
+            records.append(record)
+    return pd.DataFrame(records)
+
+
 if __name__ == "__main__":
     load_dotenv()
     db_conn = get_connection()
     plant_data = get_data(db_conn)
     plant_ids = get_all_plant_ids(plant_data["plant"])
+    plant_record = get_records_for_id(
+        plant_ids[4], plant_data["recording"])
+    print(plant_record)

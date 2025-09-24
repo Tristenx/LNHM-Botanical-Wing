51e6edbf1096624234d1f7461ebfc9ce38e1d642
Cameron <trainee.cameron.riley@sigmalabs.co.uk>
2025-09-24 17:27:35 +0100

    db.username error fix

--- api_etl_pipeline/extract-from-api/load_plants.py
+++ api_etl_pipeline/extract-from-api/load_plants.py
@@ -16,7 +16,7 @@ load_dotenv()
 
 DB_HOST   = os.getenv("DB_HOST")
 DB_PORT   = os.getenv("DB_PORT", "1433")
-DB_USERNAME   = os.getenv("DB_USER")
+DB_USERNAME   = os.getenv("DB_USERNAME")
 DB_PASS   = os.getenv("DB_PASSWORD")
 DB_NAME   = os.getenv("DB_NAME")
 DB_SCHEMA = os.getenv("DB_SCHEMA", "alpha")
@@ -50,7 +50,7 @@ def load():
         f"DRIVER={{{DB_DRIVER}}};"
         f"SERVER={DB_HOST},{DB_PORT};"
         f"DATABASE={DB_NAME};"
-        f"UID={DB_USER};"
+        f"UID={DB_USERNAME};"
         f"PWD={DB_PASS};"
         "Encrypt=yes;TrustServerCertificate=yes;Connection Timeout=30;"
     )

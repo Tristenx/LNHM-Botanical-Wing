1f7f56004558b486489bb7aa7354edb892e7875a
TristenX <1604tristenarron@gmail.com>
2025-09-26 14:00:03 +0100

    Updated moisture level

--- email_notification/trigger_step/trigger_step.py
+++ email_notification/trigger_step/trigger_step.py
@@ -47,7 +47,7 @@ def trigger_step_function(emergency_details: dict[str:str]) -> None:
 
 def good_moisture_level(moisture: float) -> bool:
     """Checks if the moisture level is acceptable for a recording."""
-    if moisture > 25:
+    if moisture > 20:
         return True
     return False
 
